ea683c3ac31b2e6706cb3115e7df22629eebd0e1
akshatparashar22 <akshatparashar2001@email.com>
2026-08-20 15:22:17 +0530

    feat(backend): add web socket, currently unauthenticated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { createServer } from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
 
 dotenv.config();
 
 const app = express();
+if (!process.env) {
+  console.error('Environment variables are not defined or file not found');
+}
+if (!process.env.PORT) {
+  console.error('PORT is not defined in the environment variables');
+}
 const port = process.env.PORT || 3000;
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +24,30 @@ app.get('/health-check', (req: Request, res: Response) => {
   res.json({ message: 'backend says hello' });
 });
 
-app.listen(port, () => {
+wss.on('connection', (ws: WebSocket) => {
+  console.log('Client connected. Total clients:', wss.clients.size);
+  // stimulating live data by incrementing a counter and sending it to the client every second
+  let counter = 0;
+  setInterval(() => {
+    counter++;
+    ws.send(`Server counter: ${counter}`);
+  }, 5000);
+
+  ws.on('message', (message: string) => {
+    console.log(`Received message: ${message}`);
+
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        counter++;
+        client.send(`Server counter: ${counter}`);
+      }
+    });
+  });
+  ws.on('close', () => {
+    console.log('Client disconnected. Total clients:', wss.clients.size);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
